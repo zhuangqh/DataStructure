1888022c0fe5d421d15b2355b357106028fd01e6
zhuangqh <zhuangqhc@163.com>
2015-11-29 20:19:07 +0800

    add detailed explanation

--- quickSort.cpp
+++ quickSort.cpp
@@ -7,23 +7,24 @@ using namespace std;
 template <typename Comparable>
 int partition(vector<int>& a, int low, int high) {
   int pivot = a[low];
-  int i = low, j = low - 1;
-  for (; i < high; ++i) {
-    if (a[i] <= pivot) {
-      ++j;
-      if (i != j) swap(a[i], a[j]);
+  int i = low + 1,    // used to scan through the list
+    last_small = low;  // last one which smaller than pivot
+  for (; i <= high; ++i) {
+    if (a[i] < pivot) {
+      ++last_small;
+      swap(a[i], a[last_small]);
     }
   }
 
-  swap(a[j+1], a[low]);
+  swap(a[last_small], a[low]);
 
-  return j + 1;
+  return last_small;
 }
 
 template <typename Comparable>
 void quickSort(vector<Comparable>& a, int low, int high) {
   if (low < high) {
-    int index = partition(a, low, high);
+    int index = partition<Comparable>(a, low, high);
     quickSort(a, low, index - 1);
     quickSort(a, index + 1, high);
   }
