3449e5f49c9c302dc2d42cb341bd89690094988c
zhuangqh <zhuangqhc@163.com>
2015-11-29 19:58:08 +0800

    add detailed explanation

--- heapSort.cpp
+++ heapSort.cpp
@@ -11,6 +11,7 @@ void percDown(vector<Comparable>& a, int i, int n) {
     child = 2 * i + 1;
     if (child != n -1 && a[child] < a[child + 1])
       ++child;
+    // find the maximum
     if (tmp < a[child])
       a[i] = a[child];
     else
@@ -22,11 +23,12 @@ void percDown(vector<Comparable>& a, int i, int n) {
 // increasing sorted order
 template <typename Comparable>
 void heapSort(vector<Comparable>& a) {
-  // build heap
+  // build heap from bottom to top
+  // the index of first node with leaf is size/2-1
   for (int i = a.size() / 2 - 1; i >= 0; --i)
     percDown(a, i, a.size());
 
-  // delete max
+  // select the max one and adjust the maxHeap
   for (int j = a.size() - 1; j > 0; --j) {
     swap(a[0], a[j]);
     percDown(a, 0, j);
